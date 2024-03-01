0c5fe8fd067eec7f632dcf041e536988b9f6ec25
King-xxz <1853091038@qq.com>
2024-03-01 20:36:11 +0800

    Add files via upload

---
+++ test_01_21/test_01_21/test.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main() {
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//	{
+//		printf("你还未成年\n");
+//		printf("要好好学习\n");
+//	}
+//	else
+//	{
+//		printf("你已成年，可以爱爱了\n");
+//	}
+//	return 0;
+//}
+
+//int main() {
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num == 0)
+//		printf("输入的数字是0");
+//	else
+//	{
+//		if (num > 0)
+//			printf("输入的数字是正数");
+//		else
+//			printf("输入的数字是负数");
+//	}
+//
+//
+//	return 0;
+//}
+//int main() {
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num == 0)
+//		printf("输入的数字是0");
+//	else if (num > 0)
+//		printf("输入的数字是正数");
+//	else
+//		printf("输入的数字是负数");
+//
+//
+//
+//return 0;
+//}
+//int main() {
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//		printf("少年\n");
+//	else if (age <= 44)
+//		printf("青年");
+//	else if (age <=59)
+//		printf("中老年");
+//	else if (age <=89)
+//		printf("老年");
+//	else
+//		printf("老寿星");
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+		if (b == 2)
+			printf("hehe\n");
+		else
+			printf("haha\n");
+	else
+			printf("dui");
+	return 0;
+}
+
+
+
+
+
+
